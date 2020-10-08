62be6f563f9fdd7d0b6b64a8995a20115ed92032
Juliette Gondon <juliettegondon@gmail.com>
2020-10-07 23:57:25 -0400

    created prompts using inquirer

--- Develop/app.js
+++ Develop/app.js
@@ -1,15 +1,102 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+//node packages
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
+
+//global variables for CLI input
+let name;
+let id;
+let email;
+let jobtitle;
+let teammembers;
+
+//main employee questions to be asked in command line:
+function employeePrompts() {
+    inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: `What is the employee's name?`,
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: `what is the employee's id?`,
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: `what is the employee's email adress?`,
+    }, 
+    {
+        type: 'input',
+        name: 'jobtitle',
+        message: `what is the employee's job title?`,
+    },
+    {
+        type: 'list',
+        name: 'jobtitle',
+        message: `what is the employee's job title?`,
+        choices: ['manager', 'engineer', 'intern']
+    }
+])}
+
+//manager questions to be asked in command line:
+function managerPrompts() {
+    inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'officenumber',
+        message: 'what is the office #?',
+    }
+])}
+//engineer questions to be asked in command line:
+function engineerPrompts() {
+    inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'github',
+        message: `what is the employee's github username?`,
+    }
+])}
+
+//intern questions to be asked in command line:
+function internPrompts() {
+    inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'school',
+        message: `where does the intern study?`,
+    }
+])}
+
+// function to initialize program
+function init() {
+    inquirer
+      .prompt(questions)
+      .then(answers => {
+        console.log('ANSWERS--->',answers);
+    })
+    };
+
+    // function call to initialize program
+init();
+
+//not sure what these are but they were included in starter code
+/* const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+ */
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
