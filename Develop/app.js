91b46d4c63c3334e1258e19ce8c720a2fa630e76
Juliette Gondon <juliettegondon@gmail.com>
2020-10-13 20:35:31 -0400

    questions working in command linegit add app.jsgit add app.js

--- Develop/app.js
+++ Develop/app.js
@@ -19,86 +19,96 @@ const render = require("./lib/htmlRenderer");
 
 let team = [];
 
-
+//manager questions to be asked in command line:
+function managerQs(){    
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'mName',
+        message: `what is the manager's name?`
+    }, 
+    {
+        type: 'input',
+        name: 'mEmail',
+        message: `what is the manager's email`
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'what is the office #?',
+    }
+]) .then(function (managerA){
+    const manager = new Manager(
+        managerA.mName,
+        managerA.mEmail,
+        managerA.officeNumber,
+        "Manager"
+    );
+        team.push(manager);
+        finishTeam();
+})
+}
 //main employee questions to be asked in command line:
-    function mainEmployeeQ() {
-        inquirer.prompt([
+   function employeeQs(){
+    inquirer.prompt([
         {
             type: 'list',
             name: 'jobTitle',
             message: `what is the employee's job title?`,
             choices: ['engineer', 'intern', 'manager']
-        },
-        {
-            type: 'input',
-            name: 'name',
-            message: `What is the employee's name?`,
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: `what is the employee's id?`,
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: `what is the employee's email adress?`,
         }
-
-    ]) .then((answers) => {
-       if (answers.jobTitle === 'manager') {
-            managerQs();
-       } else {
-        employeeQs();
-       }
-       }
-);
+]).then(function(responses){
+    switch (responses.jobTitle) {
+        case "engineer": 
+        engineerPrompts();
+        break;
+        case "intern":
+        internPrompts();
+        break;
+        case "manager":
+        managerQs();
+        break;
+        default:
     }
-        /////
-    //manager questions to be asked in command line:
-    function managerQs(){inquirer.prompt([
-        {
-            type: 'input',
-            name: 'mName',
-            message: `what is the manager's name?`
-        }, 
-        {
-            type: 'input',
-            name: 'mEmail',
-            message: `what is the manager's email`
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: 'what is the office #?',
-        }
-    ])
-        .then((answers) => {
-            console.log('ANSWERS--->', answers.mName, answers.mEmail, answers.officeNumber);
-            let managerCard = new Manager (answers.mName, answers.mEmail, answers.officeNumber);
-        }); 
-    }
-/////////
-
-      /*   (answers => {
-         let name = answers.name;
-            let id = answers.id;
-            let email = answers.email; // CREATE IF STATEMENT FOR JOBTITLE SPECIFIC Q'S FOR ENGINEER & INTERN
-            console.log('ANSWERS--->', answers, name, id, email)
-    }) */
+})
 ;
-
+   }
 
 //engineer questions to be asked in command line:
 function engineerPrompts() {
-    inquirer
-    .prompt([
+    inquirer.prompt([
     {
         type: 'input',
         name: 'github',
-        message: `what is the employee's github username?`,
+        message: `what is the github username?`,
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: `What is the engineer's name?`,
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: `what is the engineer's id?`,
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: `what is the engineer's email adress?`,
     }
-])}
+    ]) .then(function (engineerA){
+    const engineer = new Engineer(
+        engineerA.github,
+        engineerA.name,
+        engineerA.id,
+        engineerA.email,
+        "Engineer"
+    );
+        team.push(engineer);
+        finishTeam();
+})
+}
 
 //intern questions to be asked in command line:
 function internPrompts() {
@@ -108,10 +118,76 @@ function internPrompts() {
         type: 'input',
         name: 'school',
         message: `where does the intern study?`,
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: `What is the intern's name?`,
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: `what is the intern's id?`,
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: `what is the intern's email adress?`,
     }
 ])}
+
+const finishTeam = () => {
+    inquirer.prompt([
+        {
+        type: 'list',
+        name: 'addMore',
+        message: 'Add another employee?',
+        choices: ['add engineer', 'add intern', 'no more']
+        }
+    ])
+        .then(function(finish){
+            switch (finish.addMore) {
+                case "add engineer": 
+                engineerPrompts();
+                break;
+                case "add intern":
+                internPrompts();
+                break;
+                default:
+                    createTemplate();
+            
+            }
+        });
+};
+
+function createTemplate(){
+    fs.writeFileSync(outputPath, render(team));
+}
     // function call to initialize program !!!!!!!!!!????!!!!!!
-/* init();
+employeeQs();
 
- */
 
+
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
